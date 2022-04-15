398934c77d368a7444ec7bb2e8abfdb67a3d3e93
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-04-15 23:52:50 +0200

    Created a loginController.

---
+++ website-backend-spring-boot/src/main/java/no/ntnu/websitebackendspringboot/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package no.ntnu.websitebackendspringboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author "https://github.com/iHateThisName"
+ * @version 1.0
+ */
+@Controller
+public class LoginController {
+
+  @GetMapping(value = "login")
+  public String getLogin() {
+    return "login";
+  }
+}
