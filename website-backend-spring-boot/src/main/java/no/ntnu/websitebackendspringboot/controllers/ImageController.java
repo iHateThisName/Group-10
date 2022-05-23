82e45fa9e4dafc820b5e7b62691bd7f6768edac8
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-05-23 14:24:35 +0200

    Controllers
    Added a image and product Controller.

---
+++ website-backend-spring-boot/src/main/java/no/ntnu/websitebackendspringboot/controllers/ImageController.java
@@ -0,0 +1,75 @@
+package no.ntnu.websitebackendspringboot.controllers;
+
+import no.ntnu.websitebackendspringboot.models.Image;
+import no.ntnu.websitebackendspringboot.services.ImageService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Controller for image handling endpoints
+ */
+@Controller
+@CrossOrigin //enables cross-origin resource sharing
+public class ImageController {
+
+    final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+
+    /**
+     * Return image content from the database
+     *
+     * @param id ID of the image to locate
+     * @return Image content (and correct content type) or NOT FOUND
+     */
+    @GetMapping("/images/{id}")
+    public ResponseEntity<byte[]> get(@PathVariable Integer id) {
+        ResponseEntity<byte[]> response;
+        Image image = imageService.getById(id);
+        if (image != null) {
+            response = ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, image.getContentType())
+                    .body(image.getData());
+        } else {
+            response = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return response;
+    }
+
+    @PostMapping("/images")
+    public ResponseEntity<String> upload(@RequestParam("fileContent") MultipartFile multipartFile) {
+
+        ResponseEntity<String> response = null;
+        int imageId = imageService.save(multipartFile);
+        if (imageId > 0) {
+            response = new ResponseEntity<>("" + imageId, HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
+    /**
+     * Delete image content from the database
+     *
+     * @param id ID of the image to delete
+     * @return HTTP OK on success, NOT FOUND when image not found
+     */
+    @DeleteMapping("/images/{id}")
+    public ResponseEntity<String> delete(@PathVariable Integer id) {
+        ResponseEntity<String> response;
+        if (imageService.delete(id)) {
+            response = ResponseEntity.ok("");
+        } else {
+            response = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return response;
+    }
+}
---
+++ website-backend-spring-boot/src/main/java/no/ntnu/websitebackendspringboot/controllers/ProductController.java
@@ -0,0 +1,98 @@
+package no.ntnu.websitebackendspringboot.controllers;
+
+import no.ntnu.websitebackendspringboot.models.Product;
+import no.ntnu.websitebackendspringboot.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * A REST API for products
+ */
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+    private final Logger log = LoggerFactory.getLogger(getClass().getName());
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    /**
+     * Get all products stored in the database
+     *
+     * @return List of all products
+     */
+    @GetMapping("/products")
+    public List<Product> getAll() {
+        try {
+            Thread.sleep(2000); // Imitate long loading of data...
+        } catch (InterruptedException e) {
+
+            log.error("Product-loading thread interrupted!");
+        }
+        return productService.getAll();
+    }
+
+    /**
+     * Add a new product to the database. Called on HTTP POST.
+     *
+     * @param product Product to add
+     * @return HTTP OK on success and product ID in the body, BAD REQUEST on error
+     */
+    @PostMapping("/products")
+    public ResponseEntity<String> add(@RequestBody Product product) {
+        ResponseEntity<String> response;
+        String errorMessage = productService.add(product);
+        if (errorMessage == null) {
+            response = new ResponseEntity<>("" + product.getId(), HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
+    /**
+     * Update a product int the database. Called on HTTP PUT.
+     *
+     * @param id      ID of the product to update
+     * @param product Product data
+     * @return HTTP OK on success, BAD REQUEST on error
+     */
+    @PutMapping("/products/{id}")
+    public ResponseEntity<String> update(@RequestBody Product product, @PathVariable Integer id) {
+        ResponseEntity<String> response;
+        String errorMessage = productService.update(id, product);
+        if (errorMessage == null) {
+            response = new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+
+    /**
+     * Delete a product from the database. Called on HTTP DELETE.
+     *
+     * @param id ID of the product to delete
+     * @return HTTP OK on success, BAD REQUEST on error
+     */
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<String> delete(@PathVariable Integer id) {
+        ResponseEntity<String> response;
+        String errorMessage = productService.delete(id);
+        if (errorMessage == null) {
+            response = new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            response = new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+}
