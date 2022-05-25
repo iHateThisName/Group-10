b2b4fe83616577c4f048aa3f56016680152042f1
Vegard Karlstrøm <vegard_overaas@hotmail.com>
2022-05-25 15:36:02 +0200

    Added product data to json file

---
+++ website-backend-spring-boot/src/main/resources/templates/products.js
@@ -0,0 +1,51 @@
+// products (product ID : data)
+var products = {
+    123: {
+        name : "SALOMON",
+        desc : "E9 Premium Backpack",
+        img : "qJTbRxF.png",
+        price : 599
+    },
+    124: {
+        name : "MAMMUT",
+        desc : "X100 Hiking Boots 2022",
+        img : "xjhKakE.png",
+        price : 899
+    },
+    125: {
+        name : "BERGANS",
+        desc : "Allweather Jacket",
+        img : "NdXQjPx.png",
+        price : 999
+    },
+    126: {
+        name : "DEVOLD",
+        desc : "Winter Sweater 2021",
+        img : "vwNnW8s (1) (2).png",
+        price : 499
+    },
+    127: {
+        name : "NIKEDDD",
+        desc : "Dog Clothes set",
+        img : "vwNnW8s (1) (2).png",
+        price : 700
+    },
+    128: {
+        name : "BERGANS",
+        desc : "Winter Hat",
+        img : "vwNnW8s (1) (2).png",
+        price : 200
+    },
+    129: {
+        name : "NIKEDDD",
+        desc : "Water Bottle",
+        img : "vwNnW8s (1) (2).png",
+        price : 120
+    },
+    130: {
+        name : "DEVOLD",
+        desc : "Fleece Jumper 2021",
+        img : "vwNnW8s (1) (2).png",
+        price : 800
+    }
+};
