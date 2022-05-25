8ecb8b6df7d406898e1ce094abb9e3373091992e
vegard-ok <vegard_overaas@hotmail.com>
2022-05-25 13:02:01 +0200

    Trying to figure out shopping cart

--- website-backend-spring-boot/src/main/resources/templates/shoppingcart.js
+++ website-backend-spring-boot/src/main/resources/templates/shoppingcart.js
@@ -1,55 +1,10 @@
-// variables
+// show cart
 
-const cartBtn = document.querySelector('.cart-btn')
-const closeCartBtn = document.querySelector('.close-cart')
-const clearCartBtn = document.querySelector('.clear-cart')
-const cartDOM = document.querySelector('.cart')
-const cartOverlay = document.querySelector('.cart-overlay')
-const cartItems = document.querySelector('.cart-items')
-const cartTotal = document.querySelector('.cart-total')
-const cartContent = document.querySelector('.cart-content')
-const productsDOM = document.querySelector('.products-center')
+(function(){
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
 
-// shopping cart
-let cart = [];
-
-// responsible for getting the products
-class Products {
-    async getProducts() {
-        try {
-            let result = await fetch("/templates/products.json")
-            let data = await result.json()
-
-            let products = data.items;
-            products = products.map(item => {
-                const { title, price } = item.fields;
-                const { id } = item.sys
-                const image = item.fields.image.fields.file.url;
-                return { title, price, id, image}
-            })
-            return products;
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }
-}
-
-// display products - responsible for getting all the items that are being returned from the products and displaying them
-class UI {
-
-}
-
-// local storage
-class Storage {
-
-}
-
-// creates instances of the classes
-document.addEventListener("DOMContentLoaded", ()=>{
-    const ui = new UI();
-    const products = new Products();
-
-    // get all products
-    products.getProducts().then(data => console.log(data));
-})
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    })
+})();
