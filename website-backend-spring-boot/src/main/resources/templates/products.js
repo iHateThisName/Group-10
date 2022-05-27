7bfb2e9b8f85dfdc6194639c47ce16a60558ebf0
Steffen Watt Gravdehaug <73113789+Steffewa@users.noreply.github.com>
2022-05-27 15:30:47 +0200

    added products to the backend, aslo renamed images for products to make them easier to work with

--- website-backend-spring-boot/src/main/resources/templates/products.js
+++ website-backend-spring-boot/src/main/resources/templates/products.js
@@ -3,49 +3,49 @@ var products = {
     123: {
         name : "SALOMON",
         desc : "E9 Premium Backpack",
-        img : "qJTbRxF.png",
+        img : "SALOMON_Backpack.png",
         price : 599
     },
     124: {
         name : "MAMMUT",
         desc : "X100 Hiking Boots",
-        img : "xjhKakE.png",
+        img : "MAMMUT_Shoes.png",
         price : 899
     },
     125: {
         name : "BERGANS",
         desc : "Allweather Jacket",
-        img : "NdXQjPx.png",
+        img : "BERGANS_Jacket.png",
         price : 999
     },
     126: {
         name : "DEVOLD",
         desc : "Winter Sweater",
-        img : "vwNnW8s (1) (2).png",
+        img : "DEVOLD_WinterSweater.png",
         price : 499
     },
     127: {
         name : "NIKE",
         desc : "Dog Clothes set",
-        img : "vwNnW8s (1) (2).png",
+        img : "DEVOLD_WinterSweater.png",
         price : 700
     },
     128: {
         name : "BERGANS",
         desc : "Winter Hat",
-        img : "vwNnW8s (1) (2).png",
+        img : "DEVOLD_WinterSweater.png",
         price : 200
     },
     129: {
         name : "NIKE",
         desc : "Water Bottle",
-        img : "vwNnW8s (1) (2).png",
+        img : "DEVOLD_WinterSweater.png",
         price : 120
     },
     130: {
         name : "DEVOLD",
         desc : "Fleece Jumper 2021",
-        img : "vwNnW8s (1) (2).png",
+        img : "DEVOLD_WinterSweater.png",
         price : 800
     }
 };
--- website-backend-spring-boot/src/main/resources/templates/productsmen.js
+++ website-backend-spring-boot/src/main/resources/templates/productsmen.js
@@ -3,25 +3,25 @@ var productsmen = {
     223: {
         name2 : "SALOMON",
         desc2 : "E9 Premium Backpack",
-        img2 : "qJTbRxF.png",
+        img2 : "SALOMON_Backpack.png",
         price2 : 599
     },
     224: {
         name2 : "MAMMUT",
         desc2 : "X100 Hiking Boots",
-        img2 : "xjhKakE.png",
+        img2 : "MAMMUT_Shoes.png",
         price2 : 899
     },
     225: {
         name2 : "BERGANS",
         desc2 : "Allweather Jacket",
-        img2 : "NdXQjPx.png",
+        img2 : "BERGANS_Jacket.png",
         price2 : 999
     },
     226: {
         name2 : "DEVOLD",
         desc2 : "Winter Sweater",
-        img2 : "vwNnW8s (1) (2).png",
+        img2 : "DEVOLD_WinterSweater.png",
         price2 : 499
     }
 };
