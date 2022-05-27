110c82c271d73309d302c2668c0e2e499a868924
Ivar <ivabr@stud.ntnu.no>
2022-05-27 17:53:03 +0200

    Work on management page.
    Now able to view products.

--- website-backend-spring-boot/src/main/resources/static/js/Management.js
+++ website-backend-spring-boot/src/main/resources/static/js/Management.js
@@ -1,2 +1,43 @@
+const currentUrl = window.location.href;
+const productUrl = new URL(currentUrl.replace("management", "api/products"))
 
-var baseurl = ""
+function loadProducts() {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", productUrl)
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            let products = JSON.parse(xmlHttp.responseText);
+
+            let tableTop = "<table> " +
+                "<tr> + " +
+                "<th>Id</th>" +
+                "<th>Name</th>" +
+                "<th>Description</th>" +
+                "<th>Price</th> + " +
+                "</tr>";
+
+
+            let main = "";
+            for (let i = 0; i < products.length; i++) {
+
+                main += "<tr>" +
+                    "<td>" + products[i].id + "</td>" +
+                    "<td>" + products[i].name + "</td>" +
+                    "<td>" + products[i].description + "</td>" +
+                    "<td>" + products[i].price + "</td>" +
+                    "</tr>";
+            }
+
+            const tableBottom = "</table>";
+            const table = tableTop + main + tableBottom;
+            document.getElementById("root").innerHTML = table;
+
+        }
+    };
+    xmlHttp.send();
+}
+window.onload = function () {
+    loadProducts();
+}
+
+
