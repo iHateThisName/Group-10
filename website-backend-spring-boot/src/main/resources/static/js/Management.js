af60642c78243cca831fb6453a3f86c0af918c35
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-06-02 13:18:52 +0200

    Fix displaying img on the management page.

--- website-backend-spring-boot/src/main/resources/static/js/Management.js
+++ website-backend-spring-boot/src/main/resources/static/js/Management.js
@@ -24,7 +24,7 @@ function loadProducts() {
 
                 //Image
                 let image = clonedCard.querySelector(".productCardImage");
-                image.src = imageUrl + products[i].imageId;
+                image.src = imageUrl + products[i].images[0].imageId;
 
                 //Title
                 let title = clonedCard.querySelector(".productCardName");
