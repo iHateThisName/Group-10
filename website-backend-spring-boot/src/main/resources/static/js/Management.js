309fc94554070bb2206afcfbb1924938f5adfe64
Ivar Bredeli <ivabr@stud.ntnu.no>
2022-06-06 21:04:09 +0200

    Changes to management page.

--- website-backend-spring-boot/src/main/resources/static/js/Management.js
+++ website-backend-spring-boot/src/main/resources/static/js/Management.js
@@ -1,8 +1,11 @@
 const currentUrl = window.location.href;
 const productUrl = new URL(currentUrl.replace("management", "api/products"))
 const imageUrl = new URL(currentUrl.replace("management", "api/images/"))
+let card = document.querySelector(".productCard");
+
 
 window.onload = function () {
+
     loadProducts();
 }
 
@@ -15,7 +18,6 @@ function loadProducts() {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
 
             let products = JSON.parse(xmlHttp.responseText);
-            let card = document.querySelector(".productCard");
 
             for (let i = 0; i < products.length; i++) {
 
@@ -38,6 +40,10 @@ function loadProducts() {
                 let description = clonedCard.querySelector(".productCardDescription");
                 description.innerHTML = products[i].description;
 
+                //Action Button
+                let edit = clonedCard.querySelector(".actionButtonEdit");
+                edit.addEventListener("click", editClick(this, products[i].id))
+
                 //Checks if is the first loop
                 if (i === 0) {
                     //we want to replace the cloned element
@@ -52,6 +58,55 @@ function loadProducts() {
             }
         }
     };
+}
+
+function editClick(event, productId) {
+
+}
+
+function addClick() {
+    console.log("Add button was pressed")
+
+    let addCard = document.querySelector(".productAddCard");
+
+    //We make a clone of the .productAddCard element
+    let clonedAddCard = addCard.cloneNode(true);
+    clonedAddCard.style.display = "inherit";
+
+    clonedAddCard.id = "displayedAddProduct"
+
+    document.querySelector("#root")
+        .insertBefore(clonedAddCard, (document.getElementById("addProductCard")));
+
+    document.getElementById("addProductCard").style.display = "none"
+
+}
+
+function inputImage(event) {
+
+    if (event.target.files.length > 0) {
+        let chosenImage = event.target.files[0];
+        let src = URL.createObjectURL(chosenImage)
+
+
+        console.log("Image changed");
+
+        let previewImageElement = document.createElement("img")
+        previewImageElement.src = src;
+        previewImageElement.style.display = "block";
+
+        let addCard = document.getElementById("displayedAddProduct");
+        let previewDiv = addCard.querySelector(".image-editor-preview")
+
+        while (previewDiv.firstChild) {
+            previewDiv.removeChild(previewDiv.lastChild)
+        }
+
+        previewDiv.appendChild(previewImageElement);
+
+
+    }
+
 }
 
 
