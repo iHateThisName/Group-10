5ff1fa270f0e472b086fd9179762817920105407
Daniel Bjørdal <danbjo2001@gmail.com>
2022-06-07 17:26:44 +0200

    added logout button to profile page

--- website-backend-spring-boot/src/main/java/no/ntnu/websitebackendspringboot/repositories/RoleRepository.java
+++ website-backend-spring-boot/src/main/java/no/ntnu/websitebackendspringboot/repositories/RoleRepository.java
@@ -3,6 +3,7 @@ package no.ntnu.websitebackendspringboot.repositories;
 import java.util.Optional;
 import no.ntnu.websitebackendspringboot.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author "https://github.com/iHateThisName/Group-10"
